892dec70e91986bca4c1cd93bd2e328bf7f7c6b7
UsamaEjaz0 <uejaz0@gmail.com>
2020-08-28 02:08:36 +0500

    autoCommit 20200828. 20836

--- Edit_File.py
+++ Edit_File.py
@@ -1,5 +1,11 @@
+import time
+
+import subprocess
+
 
 for i in range(200000):
     f = open("Chalo_Jee.txt", "a")
     f.write("Kacha Paparr, Pakka Papar, ")
     f.close()
+    time.sleep(2)
+    subprocess.call([r'C:\Users\uejaz\Desktop\Projects\Git_Chalakiyan\AutomateGit.bat'])
